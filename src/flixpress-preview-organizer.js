64a3b6300783d00dc1c505fce7fd123c9add1f5a
Don Denton <don@happycollision.com>
2015-04-23 14:35:38 -0500

    defines items, adds setting for number of recent items

--- src/flixpress-preview-organizer.js
+++ src/flixpress-preview-organizer.js
@@ -6,10 +6,14 @@
  * Licensed under the MIT license.
  */
 (function ($) {
+  var numRecentItems = 4;
   $.fn.flixpressPreviewOrganizer = function () {
     return this.each(function () {
       // Do something to each selected element.
       var $module = $(this);
+      var $items = $module.find('.OrderItemDiv');
+      var $recentItems = $items.slice(0, numRecentItems);
+      var $pastItems = $items.slice(numRecentItems);
 
       // Get rid of pagination display
       $module.find('.RadDataPager').remove();
